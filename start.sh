1b7788b9eb30e985d6bdfe9c31ca55caf46bfd56
afoddkl <105531754+afoddkl@users.noreply.github.com>
2022-11-07 05:45:52 -0800

    Update start.sh

--- start.sh
+++ start.sh
@@ -4,7 +4,7 @@
 export UUID=aad62a03-a1aa-4b5b-b17e-262d517c1b49
 
 #请修改为自己设置的伪装站，不要带https://
-export ProxySite=www.holehike.com
+export ProxySite=www.bing.com
 
 #端口，如无特殊需求请勿更改,如果要改请一并修改dockerfile中的端口
 export Port=8080
